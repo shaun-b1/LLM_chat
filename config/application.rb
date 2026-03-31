0ad1419fbc7b6db1d3130d1d2c39e02927ae3d70
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2026-03-31 22:38:24 +1100

    preparing for rspec tests

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,9 @@ module LlmChat
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
