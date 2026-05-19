43f52528e8a83d26dc33f825a3607fe0996adc3c
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2026-05-19 22:49:44 +1000

    All gems set up, RegistrationsController and SessionsController working in Postman

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [ :create ]
+
+  def create
+    render json: UserSerializer.new(current_user).serializable_hash, status: :ok
+  end
+
+  def destroy
+    sign_out(current_user)
+  end
+
+  def respond_to_on_destroy(non_navigational_status: :no_content)
+    if current_user
+      head :no_content
+    else
+      head :unauthorized
+    end
+  end
 end
